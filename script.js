51b39b7baa8fa5ce4d77d07505c5b6d4661a25fd
Jasper <halvorsonjasper11@gmail.com>
2024-05-25 11:54:59 -0700

    Make interactive

--- script.js
+++ script.js
@@ -34,38 +34,168 @@ const svg = d3.select("body").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+
+let i = 0;
+const duration = 750;
+
 // Create tree
 const treemap = d3.tree().size([height, width]);
 
 // Add parent, children, height, depth to d3
-const root = d3.hierarchy(data, d => d.children);
-treemap(root);
+let root = d3.hierarchy(data, function(d) { return d.children; });
+root.x0 = height / 2;
+root.y0 = 0;
 
-const nodes = root.descendants();
-const links = root.links();
+// Collapse after the second level
+root.children.forEach(collapse);
 
-// Create the links between the nodes
-svg.selectAll('path.link')
-  .data(links)
-  .enter().append('path')
-  .attr('class', 'link')
-  .attr('d', d3.linkHorizontal()
-      .x(d => d.y)
-      .y(d => d.x));
+update(root);
 
-// Create nodews
-const node = svg.selectAll('g.node')
-  .data(nodes)
-  .enter().append('g')
-  .attr('class', 'node')
-  .attr('transform', d => `translate(${d.y},${d.x})`);
+// Collapse the node and all its children
+function collapse(d) {
+  if(d.children) {
+    d._children = d.children;
+    d._children.forEach(collapse);
+    d.children = null;
+  }
+}
 
-// make nodes black cirlces and add labels
-node.append('circle')
-  .attr('r', 5);
+function update(source) {
 
-node.append('text')
-  .attr('dy', '.35em')
-  .attr('x', d => d.children ? -10 : 10)
-  .attr('text-anchor', d => d.children ? 'end' : 'start')
-  .text(d => d.data.name);
+    // Assigns the x and y position for the nodes
+    const treeData = treemap(root);
+  
+    // Compute the new tree layout.
+    const nodes = treeData.descendants(),
+          links = treeData.descendants().slice(1);
+  
+    // Normalize for fixed-depth.
+    nodes.forEach(function(d){ d.y = d.depth * 180});
+  
+    // ****************** Nodes section ***************************
+  
+    // Update the nodes...
+    const node = svg.selectAll('g.node')
+        .data(nodes, function(d) { return d.id || (d.id = ++i); });
+  
+    // Enter any new modes at the parent's previous position.
+    const nodeEnter = node.enter().append('g')
+        .attr('class', 'node')
+        .attr("transform", function(d) {
+            return "translate(" + source.y0 + "," + source.x0 + ")";
+        })
+        .on('click', click);
+  
+    // make nodes black cirlces and add labels
+    nodeEnter.append('circle')
+        .attr('class', 'node')
+        .attr('r', 1e-6)
+        .style("fill", function(d) {
+            return d._children ? "lightsteelblue" : "#fff";
+        });
+  
+    nodeEnter.append('text')
+        .attr("dy", ".35em")
+        .attr("x", function(d) {
+            return d.children || d._children ? -13 : 13;
+        })
+        .attr("text-anchor", function(d) {
+            return d.children || d._children ? "end" : "start";
+        })
+        .text(function(d) { return d.data.name; });
+  
+    // UPDATE
+    const nodeUpdate = nodeEnter.merge(node);
+  
+    // Transition to the proper position for the node
+    nodeUpdate.transition()
+      .duration(duration)
+      .attr("transform", function(d) { 
+          return "translate(" + d.y + "," + d.x + ")";
+       });
+  
+    // Update the node attributes and style
+    nodeUpdate.select('circle.node')
+      .attr('r', 10)
+      .style("fill", function(d) {
+          return d._children ? "lightsteelblue" : "#fff";
+      })
+      .attr('cursor', 'pointer');
+  
+    // Remove any exiting nodes
+    const nodeExit = node.exit().transition()
+        .duration(duration)
+        .attr("transform", function(d) {
+            return "translate(" + source.y + "," + source.x + ")";
+        })
+        .remove();
+  
+    // On exit reduce the node circles size to 0
+    nodeExit.select('circle')
+      .attr('r', 1e-6);
+  
+    // On exit reduce the opacity of text labels
+    nodeExit.select('text')
+      .style('fill-opacity', 1e-6);
+  
+    
+
+    //LINKS
+  
+    // Update the links...
+    const link = svg.selectAll('path.link')
+        .data(links, function(d) { return d.id; });
+  
+    // Enter any new links at the parent's previous position.
+    const linkEnter = link.enter().insert('path', "g")
+        .attr("class", "link")
+        .attr('d', function(d){
+          const o = {x: source.x0, y: source.y0}
+          return diagonal(o, o)
+        });
+  
+    // UPDATE
+    const linkUpdate = linkEnter.merge(link);
+  
+    // Transition back to the parent element position
+    linkUpdate.transition()
+        .duration(duration)
+        .attr('d', function(d){ return diagonal(d, d.parent) });
+  
+    // Remove any exiting links
+    const linkExit = link.exit().transition()
+        .duration(duration)
+        .attr('d', function(d) {
+          const o = {x: source.x, y: source.y}
+          return diagonal(o, o)
+        })
+        .remove();
+  
+    // Store the old positions for transition.
+    nodes.forEach(function(d){
+      d.x0 = d.x;
+      d.y0 = d.y;
+    });
+  
+    // Creates a curved (diagonal) path from parent to the child nodes
+    function diagonal(s, d) {
+      const path = `M ${s.y} ${s.x}
+              C ${(s.y + d.y) / 2} ${s.x},
+                ${(s.y + d.y) / 2} ${d.x},
+                ${d.y} ${d.x}`;
+  
+      return path;
+    }
+  
+    // Toggle children on click.
+    function click(event, d) {
+      if (d.children) {
+          d._children = d.children;
+          d.children = null;
+      } else {
+          d.children = d._children;
+          d._children = null;
+      }
+      update(d);
+    }
+}
